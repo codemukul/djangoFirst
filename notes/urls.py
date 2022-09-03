0eb81fc878b22534fb2c59e5ef76bc4b5188ccfe
mukul <76431593+codemukul@users.noreply.github.com>
2022-09-03 17:47:03 +0530

    class based views

--- notes/urls.py
+++ notes/urls.py
@@ -4,7 +4,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('smart/',views.list),
-    path('blog/<int:pk>',views.detail),
-
+    path('smart/',views.NotesListView.as_view()),
+    # path('smart/',views.list),
+    path('blog/<int:pk>',views.NotesDetailView.as_view()),
+    # path('blog/<int:pk>',views.detail),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,17 +1,29 @@
+from msilib.schema import ListView
 from django.shortcuts import render
 from django.http import Http404
+from django.views.generic import DetailView, ListView
 
 from .models import Notes
 
-def list(request):
-    all_notes = Notes.objects.all()
-    return render(request,'notes/notes_list.html',{'notes':all_notes})
+class NotesListView(ListView):
+    model = Notes
+    context_object_name = "notes"
+    # template_name = "notes/notes_list.html" (not required because we have named as the standard name of class , so it automatically fetches it .....)
 
-def detail(request , pk):
-    try:
-        note = Notes.objects.get(pk=pk)
-    except Notes.DoesNotExist:
-        # raise Http404('notes/notes_list.html')
-        all_notes = Notes.objects.all()
-        return render(request,'notes/notes_list.html',{'notes':all_notes})
-    return render(request,'notes/notes_detail.html',{'note':note})
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = "note"
+    # template_name = "notes/notes_detail.html"
+
+# def list(request):
+#     all_notes = Notes.objects.all()
+#     return render(request,'notes/notes_list.html',{'notes':all_notes})
+
+# def detail(request , pk):
+#     try:
+#         note = Notes.objects.get(pk=pk)
+#     except Notes.DoesNotExist:
+#         # raise Http404('notes/notes_list.html')
+#         all_notes = Notes.objects.all()
+#         return render(request,'notes/notes_list.html',{'notes':all_notes})
+#     return render(request,'notes/notes_detail.html',{'note':note})
