c45b9a286753764bda06c176aa4abd39ec8128a3
mukul <76431593+codemukul@users.noreply.github.com>
2022-09-05 20:10:29 +0530

    login , logout , signup

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('' , views.Homeview.as_view()),
     path('authorized',views.Authorizedview.as_view()),
+    path('login',views.LoginInterfaceView.as_view(), name='login'),
+    path('logout',views.LogoutInterfaceView.as_view(), name='logout'),
+    path('signup',views.SignupView.as_view(), name='signup'),
 ]
--- home/views.py
+++ home/views.py
@@ -6,13 +6,32 @@ from datetime import datetime
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.views import LoginView,LogoutView
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect
 
+class SignupView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'home/signup.html'
+    success_url = '/login'
+    
+    def get(self, request ,*args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('notes.list')
+        return super().get(request, *args, **kwargs)
+
+class LoginInterfaceView(LoginView):
+    template_name = 'home/login.html'
+
+class LogoutInterfaceView(LoginRequiredMixin ,LogoutView):
+    template_name = 'home/logout.html'
 
 class Homeview(TemplateView):
     template_name = 'home/welcome.html'
     extra_context = {'today':datetime.today()}
 
-class Authorizedview(LoginRequiredMixin ,TemplateView):
+class Authorizedview(TemplateView):
     template_name = 'home/authorized.html'
     login_url = '/admin'
 
