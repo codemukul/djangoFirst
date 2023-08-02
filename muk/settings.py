200add3a719044ad42571b36614b53351fc7a93e
Mukul Kumar Gupta <76431593+codemukul@users.noreply.github.com>
2023-08-02 21:31:51 +0000

    fix: final

--- muk/settings.py
+++ muk/settings.py
@@ -135,7 +135,6 @@ STATIC_URL = 'static/'
 #     BASE_DIR / 'static',
 # ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
